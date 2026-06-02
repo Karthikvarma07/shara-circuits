4b0888af51817024ec0617a059459d5ea1b5a35e
karthik varma dasararaju <dasararajukarthikvarma@gmail.com>
2026-06-02 13:05:28 +0530

    fixed db import case

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 
-import connectDB from "./config/Db.js";
+import connectDB from "./config/db.js";
 import contactRoutes from "./routes/contactRoutes.js";
 
 dotenv.config();
